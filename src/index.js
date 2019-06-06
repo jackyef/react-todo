206992f21934bd18a8eefb8ebc78565b1df108ad
Jacky Efendi <jacky.efendi1@gmail.com>
2019-06-06 14:19:43 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="container">
+      <div className="input-row">
+        <div className="align-self-start">
+          <label for="search">Search: </label>
+          <input
+            type="text"
+            name="search"
+            placeholder="enter keyword here..."
+          />
+        </div>
+        <div className="align-self-end">
+          <input type="text" placeholder="Example: read more blogs" />
+          <button className="blue">Add to list</button>
+        </div>
+      </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <div className="todo-container">
+        <div className="row bold">
+          <div>#</div>
+          <div>To-do</div>
+        </div>
+        <div className="row">
+          <div>1</div>
+          <div>Learn React</div>
+          <div>
+            <button className="green">Mark as done</button>
+          </div>
+          <div>
+            <button className="red">Delete</button>
+          </div>
+        </div>
+        <div className="row linethrough">
+          <div>2</div>
+          <div>Learn Webpack</div>
+          <div>
+            <button className="green">Mark as not done</button>
+          </div>
+          <div>
+            <button className="red">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
